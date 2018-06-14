7ed76310f71a2edb4a7817a52041a1ed005a95d2
Sam Zhao <zhao.shengjun@gmail.com>
2018-06-14 23:05:20 +1000

    decorate car with options

--- src/decorator.ts
+++ src/decorator.ts
@@ -18,3 +18,35 @@ class ModelX extends Car {
     return 73000;
   }
 }
+
+abstract class CarOptions extends Car {
+  decoratedCar: Car;
+  public abstract getDescription(): string;
+  public abstract cost(): number;
+}
+
+class EnhancedAutoPilot extends CarOptions {
+  constructor(car: Car) {
+    super();
+    this.decoratedCar = car;
+  }
+  public getDescription(): string {
+    return this.decoratedCar.getDescription() + ", Enhanced AutoPilot";
+  }
+  public cost(): number {
+    return this.decoratedCar.cost() + 5000;
+  }
+}
+
+class RearFacingSeats extends CarOptions {
+  constructor(car: Car) {
+    super();
+    this.decoratedCar = car;
+  }
+  public getDescription(): string {
+    return this.decoratedCar.getDescription() + ", Rear Facing Seats";
+  }
+  public cost(): number {
+    return this.decoratedCar.cost() + 7000;
+  }
+}
