edbf121dd3b830a3484752ea659e0ac25a5bc392
Sam Zhao <zhao.shengjun@gmail.com>
2018-06-15 22:23:36 +1000

    implement interfaces

--- src/observer.ts
+++ src/observer.ts
@@ -7,8 +7,21 @@ interface Subject {
 interface Observer {
   update(temperature: number);
 }
-class WeatherStation {
+class WeatherStation implements Subject {
   private temperature: number;
+  private observers: Observer[] = [];
+  registerObserver(o: Observer) {
+    this.observers.push(o);
+  }
+  removeObserver(o: Observer) {
+    let index = this.observers.indexOf(o);
+    this.observers.splice(index, 1);
+  }
+  notifyObservers() {
+    for (let o of this.observers) {
+      o.update(this.temperature);
+    }
+  }
 
   setTemperature(temp: number) {
     console.log("WeatherStation: new temperature: ", temp);
@@ -16,4 +29,19 @@ class WeatherStation {
   }
 }
 
-class TemperatureDisplay {}
+class TemperatureDisplay implements Observer {
+  private subject: Subject;
+  constructor(ws: Subject) {
+    this.subject = ws;
+    ws.registerObserver(this);
+  }
+  update(temperature: number) {
+    console.log("New temperature: ", temperature);
+  }
+}
+
+let ws = new WeatherStation();
+let tempDisplay = new TemperatureDisplay(ws);
+
+ws.setTemperature(20);
+ws.setTemperature(30);
