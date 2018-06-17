c72ff77e03fecbc2bb01f093a2f9855af3f90c8e
Sam Zhao <zhao.shengjun@gmail.com>
2018-06-17 22:57:58 +1000

    update order

--- src/state.ts
+++ src/state.ts
@@ -6,5 +6,14 @@ interface State {
 }
 
 class Order {
+  public currentState: State;
   constructor() {}
+
+  public setState(state: State) {
+    this.currentState = state;
+  }
+
+  public getState(): State {
+    return this.currentState;
+  }
 }
